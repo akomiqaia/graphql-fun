cdca7c0b1f11a5e2412fb7963bf68473f124d1dd
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2020-07-23 23:31:51 +0100

    configure settings file

--- hackernews/settings.py
+++ hackernews/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import django_heroku
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -23,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'k2(z0@%%-8ne%iq$-a3$o#j=$nag=s5ju%8(bq4^e#qgk_s%2!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','https://django-testapi.herokuapp.com']
 
 
 # Application definition
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'hackernews.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangogirls',
+        'USER': 'name',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -128,3 +133,6 @@ GRAPHENE = {
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+
+DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=500)
