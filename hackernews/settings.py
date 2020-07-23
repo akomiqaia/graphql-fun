dea1301430fb231023dd6c112b8d50fad899d712
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2020-07-23 23:53:44 +0100

    change debug value from true to false

--- hackernews/settings.py
+++ hackernews/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'k2(z0@%%-8ne%iq$-a3$o#j=$nag=s5ju%8(bq4^e#qgk_s%2!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','https://django-testapi.herokuapp.com']
 
